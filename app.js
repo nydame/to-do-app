69cf63fb33d026de379e98f0dfa48d4cc25233e9
felicia <felicia@hyperarts.com>
2018-01-22 14:54:20 -0800

    Implement a delete button for each list item.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 function onReady() {
     // keep track of to-do items
     let toDos = [];
+    // assign ID numbers to to-do items
+    let id = 0;
     // select DOM elements to use
-    const addToDoForm = document.getElementById("addToDoForm");
-    const newToDoText = document.getElementById("newToDoText");
-    const toDoList = document.getElementById("toDoList");
+    const addToDoForm = document.getElementById('addToDoForm');
+    const newToDoText = document.getElementById('newToDoText');
+    const toDoList = document.getElementById('toDoList');
     // create latest to-do
     function createNewToDo() {
         // make sure entry isn't an empty string
@@ -14,36 +16,56 @@ function onReady() {
         // update state
         toDos.push({
             title: newToDoText.value,
-            complete: false
+            complete: false,
+            id: id,
         });
         // reset text field
-        newToDoText.value = "";
-        // show the list
+        newToDoText.value = '';
+        // increment the ID
+        id++;
+        // finally, update the list to reflect latest data & show
         renderTheUI();
     }
-    // reset, rebuild & show the list
+    // reset, rebuild & show the list according to latest data
     function renderTheUI() {
-		// flush stale content
-		toDoList.textContent = '';
-		// re-build list
+        // flush stale content
+        toDoList.textContent = '';
+        // re-build list
         toDos.forEach(function(toDo) {
             // create a new li
-            const newLi = document.createElement("li");
+            const newLi = document.createElement('li');
             // create a new checkbox
-            const checkbox = document.createElement("input");
-            checkbox.type = "checkbox";
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
             // create a span to wrap text of to-do
-            const titleWrap = document.createElement("span");
+            const titleWrap = document.createElement('span');
             titleWrap.textContent = toDo.title;
+            // create a delete button
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Delete';
+            // provide a listener for the delete deleteButton
+            deleteButton.addEventListener('click', function(event) {
+                event.preventDefault();
+                deleteToDo(toDo.id);
+            });
             // append elements to li
             newLi.appendChild(checkbox);
             newLi.appendChild(titleWrap); // OMITTED FROM CHECKPOINT INSTRUCTIONS
+            newLi.appendChild(deleteButton);
             // finally, append li to list so that it shows up
             toDoList.appendChild(newLi);
         });
     }
+    // listen for click of delete buttons
+    function deleteToDo(id) {
+        console.log('Delete ' + id);
+        debugger;
+        toDos = toDos.filter(toDo => toDo.id !== id);
+        debugger;
+        renderTheUI();
+    }
     // listen for new to-do
-    addToDoForm.addEventListener("submit", event => {
+    addToDoForm.addEventListener('submit', event => {
         event.preventDefault();
         createNewToDo();
     });
