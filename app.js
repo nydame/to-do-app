1ecf382ceb5db3554aca9f5b90f7318b99c82e73
felicia <felicia@hyperarts.com>
2018-01-22 11:00:18 -0800

    Implement 'Delete Completed' button to clear tasks marked as complete.

--- app.js
+++ app.js
@@ -1,24 +1,67 @@
 function onReady() {
-	// select DOM elements to use
-	const addToDoForm = document.getElementById('addToDoForm');
-	const newToDoText = document.getElementById('newToDoText');
-	const toDoList = document.getElementById('toDoList');
-
-	addToDoForm.addEventListener('submit', event => {console.log('submission');debugger;
-		// prevent page reload or redirection
-		event.preventDefault();
-		// 
-		let title = newToDoText.value; console.log(title);
-		let newLi = document.createElement('li');
-		let checkbox = document.createElement('input');
-		checkbox.type = 'checkbox';
-		newLi.textContent = title;
-		newLi.appendChild(checkbox);
-		toDoList.appendChild(newLi);
-		newToDoText.value = '';
-	});
+    // select DOM elements to use
+    const addToDoForm = document.getElementById("addToDoForm");
+    const newToDoText = document.getElementById("newToDoText");
+    const toDoList = document.getElementById("toDoList");
+    const deleteCompletedButton = document.getElementById("deleteCompleted");
+    const initialState = "pending";
+    const toggleToDoState = el => {
+        // toggle class on element
+        el.classList.toggle(initialState);
+        let classNames = el.className.split(" ");
+        let ownCheckbox = el.querySelector('input[type="checkbox"]');
+        // if checkbox within el found, toggle checked state
+        if (ownCheckbox) {
+            let checked = classNames.indexOf(initialState) === -1;
+            ownCheckbox.setAttribute("checked", checked);
+        }
+    };
+    // listen for submission
+    addToDoForm.addEventListener("submit", event => {
+        // prevent page reload or redirection when Submit button is clicked
+        event.preventDefault();
+        // define elements of list item
+        let title = newToDoText.value;
+        let newLi = document.createElement("li");
+        // provide each list item with a class of "pending"
+        newLi.className = initialState;
+        // addEventListener to list item so it can toggle add or remove "pending"
+        // NB: USING ARROW FUNCTION MEANS "THIS" IS SET TO GLOBAL SCOPE!!!
+        // newLi.addEventListener('click', event => {
+        // 	toggleToDoState(this);
+        // });
+        newLi.addEventListener('click', function(event) {
+            toggleToDoState(this);
+        });
+        let checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.name = 'todo';
+        // build list item and attach to list
+        newLi.textContent = title;
+        newLi.appendChild(checkbox);
+        toDoList.appendChild(newLi);
+		// empty text field in preparation for the next entry
+        newToDoText.value = '';
+    });
+    // listen for clicking of Delete button
+    deleteCompletedButton.addEventListener("click", function(event) {
+		// why is this necessary???
+        event.preventDefault();
+		//
+        let checkedBoxes = document.querySelectorAll(
+            "#toDoList [checked=true]"
+        );
+        let completedListItems = null;
+		//
+        for (let i = 0; i < checkedBoxes.length; i++) {
+            let checkBox = checkedBoxes[i];
+            let listItemToDelete = checkBox.parentNode;
+            toDoList.removeChild(listItemToDelete);
+			// toDoList.removeChild(checkBox);
+        }
+    });
 }
 
 window.onload = function() {
-	onReady();
+    onReady();
 };
