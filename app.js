3e1d4adcbd43ae3493d923c70c4a52be6f4f8ce5
felicia <felicia@hyperarts.com>
2018-01-22 14:12:21 -0800

    Rewrite to-do app the React way, i.e., with UI as a function of state.

--- app.js
+++ app.js
@@ -1,24 +1,54 @@
 function onReady() {
-	// select DOM elements to use
-	const addToDoForm = document.getElementById('addToDoForm');
-	const newToDoText = document.getElementById('newToDoText');
-	const toDoList = document.getElementById('toDoList');
-
-	addToDoForm.addEventListener('submit', event => {console.log('submission');debugger;
-		// prevent page reload or redirection
-		event.preventDefault();
-		// 
-		let title = newToDoText.value; console.log(title);
-		let newLi = document.createElement('li');
-		let checkbox = document.createElement('input');
-		checkbox.type = 'checkbox';
-		newLi.textContent = title;
-		newLi.appendChild(checkbox);
-		toDoList.appendChild(newLi);
-		newToDoText.value = '';
-	});
+    // keep track of to-do items
+    let toDos = [];
+    // select DOM elements to use
+    const addToDoForm = document.getElementById("addToDoForm");
+    const newToDoText = document.getElementById("newToDoText");
+    const toDoList = document.getElementById("toDoList");
+    // create latest to-do
+    function createNewToDo() {
+        // make sure entry isn't an empty string
+        if (!newToDoText.value) {
+            return;
+        }
+        // update state
+        toDos.push({
+            title: newToDoText.value,
+            complete: false
+        });
+        // reset text field
+        newToDoText.value = "";
+        // show the list
+        renderTheUI();
+    }
+    // reset, rebuild & show the list
+    function renderTheUI() {
+		// flush stale content
+		toDoList.textContent = '';
+		// re-build list
+        toDos.forEach(function(toDo) {
+            // create a new li
+            const newLi = document.createElement("li");
+            // create a new checkbox
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            // create a span to wrap text of to-do
+            const titleWrap = document.createElement("span");
+            titleWrap.textContent = toDo.title;
+            // append elements to li
+            newLi.appendChild(checkbox);
+            newLi.appendChild(titleWrap); // OMITTED FROM CHECKPOINT INSTRUCTIONS
+            // finally, append li to list so that it shows up
+            toDoList.appendChild(newLi);
+        });
+    }
+    // listen for new to-do
+    addToDoForm.addEventListener("submit", event => {
+        event.preventDefault();
+        createNewToDo();
+    });
 }
 
 window.onload = function() {
-	onReady();
+    onReady();
 };
